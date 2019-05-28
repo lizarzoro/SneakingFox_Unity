6d0a685623451f46d124fbeeed21a91108ea3af3
lizarzoro <tkddnr468@gmail.com>
2019-05-28 17:55:06 +0900

    09. camera move

---
+++ Sneaking Fox/Assets/camerafollow2D.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camerafollow2D : MonoBehaviour
+{
+    public Transform target;
+    public float smooting;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
